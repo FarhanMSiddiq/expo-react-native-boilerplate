9dd2e7fd0858fa5ccc16eb137ec6a39ba87c5a67
farhan <farhan@impstudio.co.id>
2025-05-11 02:50:42 +0700

    refactor : component todo & review

--- src/app/components/ReviewCard.tsx
+++ src/app/components/ReviewCard.tsx
@@ -1,6 +1,7 @@
-import { View, Text, Platform, Dimensions } from "react-native";
+import { View, Text, Platform, Dimensions, Image } from "react-native";
 import { FC } from "react";
 import { Review } from "../../models/Review";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const { width: screenWidth } = Dimensions.get("window");
 const maxContentWidth = Platform.OS === "web" ? 300 : screenWidth;
@@ -15,12 +16,28 @@ const ReviewCard: FC<Props> = ({ review }) => {
       className="bg-white m-2 p-3 rounded-lg shadow"
       style={{ maxWidth: maxContentWidth }}
     >
-      <Text className="text-lg font-semibold text-black mb-2">
+      {/* Reviewer Info */}
+      <View className="flex-row items-center mb-3">
+        <Image
+          source={{
+            uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHfd3PPulVSp4ZbuBFNkePoUR_fLJQe474Ag&s",
+          }}
+          className="w-10 h-10 rounded-full mr-3"
+        />
+        <View>
+          <Text className="font-semibold text-base text-gray-800">
+            {"Anonymous"}
+          </Text>
+        </View>
+      </View>
+
+      {/* Title & Body */}
+      <Text className="text-lg font-semibold text-black mb-1">
         {review.title}
       </Text>
-
       <Text className="text-sm text-gray-700 mb-3">{review.body}</Text>
 
+      {/* Tags */}
       <View className="flex-row flex-wrap mb-3">
         {review.tags.map((tag, index) => (
           <View
@@ -32,6 +49,7 @@ const ReviewCard: FC<Props> = ({ review }) => {
         ))}
       </View>
 
+      {/* Reactions */}
       <View className="flex-row justify-between items-center">
         <Text className="text-xs text-gray-500">👁️ {review.views} views</Text>
         <Text className="text-xs text-gray-500">
--- src/app/components/TodoItem.tsx
+++ src/app/components/TodoItem.tsx
@@ -10,18 +10,16 @@ const TodoItem = ({ todo }: Props) => {
   const { width: screenWidth } = Dimensions.get("window");
   const maxContentWidth = Platform.OS === "web" ? 300 : screenWidth;
 
+  const baseClass =
+    "p-3 rounded-lg mb-2 " + (todo.completed ? "bg-red-100" : "bg-blue-200");
+
+  const textClass =
+    "text-base " +
+    (todo.completed ? "line-through text-gray-400" : "text-blue-900");
+
   return (
-    <View
-      className="p-3 bg-gray-100 rounded-lg mb-2"
-      style={{ maxWidth: maxContentWidth }}
-    >
-      <Text
-        className={`text-base text-gray-80 ${
-          todo.completed && "line-through text-gray-400"
-        }`}
-      >
-        {todo.todo}
-      </Text>
+    <View className={baseClass} style={{ maxWidth: maxContentWidth }}>
+      <Text className={textClass}>{todo.todo}</Text>
     </View>
   );
 };
